c6f608d634b47b58965f23af9f8d75fd223a9636
Guilherme Assemany <guilhermeassemany@hotmail.com>
2018-05-05 17:44:30 -0300

    404 on post

--- frontend/src/Components/PostPage.js
+++ frontend/src/Components/PostPage.js
@@ -12,10 +12,11 @@ import PostVote from "./PostVote"
 import PostForm from "./PostForm"
 import { withRouter } from 'react-router-dom'
 import Swal from "sweetalert2"
+import NotFoundPage from "./NotFoundPage"
 
 class PostPage extends Component {
 
-    componentDidMount() {
+    componentWillMount() {
         this.props.loadPostDetails(this.props.match.params.post_id)
     }
 
@@ -36,87 +37,88 @@ class PostPage extends Component {
 
     render() {
         const {post, editPost, error} = this.props
-        console.log(post)
-        return (
-            <Container fluid={true}>
-                <TopBar/>
-                <Row>
-                    <Col xs={12}>
-                        <h3>
-                            {post.title}
-                        </h3>
-                        <hr/>
-                    </Col>
-                    <Col xs={12} md={8}>
-                        <Card>
-                            <CardBody>
-                                <CardSubtitle>
-                                    Autor: <span className="badge badge-primary">{post.author}</span> /
-                                    Categoria: <span className="badge badge-primary">{post.category}</span>
-                                </CardSubtitle>
-                                <hr/>
-                                <CardText>
-                                    {post.body}
-                                </CardText>
-                            </CardBody>
-                        </Card>
-                    </Col>
-                    <Col xs={12} md={4}>
-                        <Card>
-                            <CardBody>
-                                <Row className='text-center'>
-                                    <Col xs={6}>
-                                        <FontAwesomeIcon icon="thumbs-up"/>
-                                        <p>{post.voteScore}</p>
-                                        Votos
-                                    </Col>
-                                    <Col xs={6} className='text-primary'>
-                                        <FontAwesomeIcon icon="comments"/>
-                                        <p>{post.commentCount}</p>
-                                        Comentários
-                                    </Col>
-                                </Row>
-                                <hr/>
-                                <Row>
-                                    <Col xs={12} className='text-center'>
-                                        <PostVote post_id={post.id}/>
-                                    </Col>
-                                </Row>
-                                <hr/>
-                                <Row className='text-center'>
-                                    <Col xs={6} className='text-primary'>
-                                        <Button block={true} outline color='info' onClick={() => this.toggle()}>
-                                            <FontAwesomeIcon icon="edit"/>
-                                            <p>Editar</p>
-                                        </Button>
-                                    </Col>
-                                    <Col xs={6} className='text-primary'>
-                                        <Button block={true} outline color='danger' onClick={() => this.deletePost(post.id)}>
-                                            <FontAwesomeIcon icon="trash"/>
-                                            <p>Deletar</p>
-                                        </Button>
-                                    </Col>
-                                </Row>
-                            </CardBody>
-                        </Card>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col xs={12} md={8}>
-                        {post.id && (
-                            <CommentList post_id={post.id} />
-                        )}
-                    </Col>
-                </Row>
-                <Modal isOpen={this.state.modal} toggle={() => this.toggle()} backdrop={true}>
-                    <ModalHeader toggle={() => this.toggle()}>Novo Post</ModalHeader>
-                    <ModalBody>
-                        <PostForm addPost={editPost} addPostCb={this.toggle} post={post}/>
-                    </ModalBody>
-                </Modal>
-            </Container>
-
-        )
+        if(post.title !== undefined) {
+            return (
+                <Container fluid={true}>
+                    <TopBar/>
+                    <Row>
+                        <Col xs={12}>
+                            <h3>
+                                {post.title}
+                            </h3>
+                            <hr/>
+                        </Col>
+                        <Col xs={12} md={8}>
+                            <Card>
+                                <CardBody>
+                                    <CardSubtitle>
+                                        Autor: <span className="badge badge-primary">{post.author}</span> /
+                                        Categoria: <span className="badge badge-primary">{post.category}</span>
+                                    </CardSubtitle>
+                                    <hr/>
+                                    <CardText>
+                                        {post.body}
+                                    </CardText>
+                                </CardBody>
+                            </Card>
+                        </Col>
+                        <Col xs={12} md={4}>
+                            <Card>
+                                <CardBody>
+                                    <Row className='text-center'>
+                                        <Col xs={6}>
+                                            <FontAwesomeIcon icon="thumbs-up"/>
+                                            <p>{post.voteScore}</p>
+                                            Votos
+                                        </Col>
+                                        <Col xs={6} className='text-primary'>
+                                            <FontAwesomeIcon icon="comments"/>
+                                            <p>{post.commentCount}</p>
+                                            Comentários
+                                        </Col>
+                                    </Row>
+                                    <hr/>
+                                    <Row>
+                                        <Col xs={12} className='text-center'>
+                                            <PostVote post_id={post.id}/>
+                                        </Col>
+                                    </Row>
+                                    <hr/>
+                                    <Row className='text-center'>
+                                        <Col xs={6} className='text-primary'>
+                                            <Button block={true} outline color='info' onClick={() => this.toggle()}>
+                                                <FontAwesomeIcon icon="edit"/>
+                                                <p>Editar</p>
+                                            </Button>
+                                        </Col>
+                                        <Col xs={6} className='text-primary'>
+                                            <Button block={true} outline color='danger' onClick={() => this.deletePost(post.id)}>
+                                                <FontAwesomeIcon icon="trash"/>
+                                                <p>Deletar</p>
+                                            </Button>
+                                        </Col>
+                                    </Row>
+                                </CardBody>
+                            </Card>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col xs={12} md={8}>
+                            {post.id && (
+                                <CommentList post_id={post.id} />
+                            )}
+                        </Col>
+                    </Row>
+                    <Modal isOpen={this.state.modal} toggle={() => this.toggle()} backdrop={true}>
+                        <ModalHeader toggle={() => this.toggle()}>Novo Post</ModalHeader>
+                        <ModalBody>
+                            <PostForm addPost={editPost} addPostCb={this.toggle} post={post}/>
+                        </ModalBody>
+                    </Modal>
+                </Container>
+            )
+        }
+        return <NotFoundPage />
     }
 }
 
